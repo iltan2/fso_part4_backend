381a0a8037bccef7957a6e6636e8b756165c9dcc
iltan2 <55798506+iltan2@users.noreply.github.com>
2023-04-13 22:59:37 +0800

    Format tests/blog.test.js

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -4,18 +4,17 @@ const app = require("../app");
 
 const api = supertest(app);
 
-const helper = require("./blog_test_helper")
+const helper = require("./blog_test_helper");
 
-const Blog = require('../models/blog')
+const Blog = require("../models/blog");
 
 beforeEach(async () => {
-  await Blog.deleteMany({})
-  let blogObject = new Blog(helper.initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(helper.initialBlogs[1])
-  await blogObject.save()
-})
-
+  await Blog.deleteMany({});
+  let blogObject = new Blog(helper.initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(helper.initialBlogs[1]);
+  await blogObject.save();
+});
 
 test("notes are returned as json", async () => {
   await api
@@ -24,40 +23,39 @@ test("notes are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
-test('all notes are returned', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+test("all notes are returned", async () => {
+  const response = await api.get("/api/blogs");
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
 
-test('a specific title is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
-  const titles = response.body.map(r => r.title)
-  expect(titles).toContain('Test Title')
-})
+test("a specific title is within the returned blogs", async () => {
+  const response = await api.get("/api/blogs");
+  const titles = response.body.map((r) => r.title);
+  expect(titles).toContain("Test Title");
+});
 
-test('a valid blog can be added', async () => {
+
+test("a valid blog can be added", async () => {
   const newBlog = {
-    title: 'new title test',
-    author: 'new author test',
-    url: 'new url test',
+    title: "new title test",
+    author: "new author test",
+    url: "new url test",
     likes: 100,
-  }
+  };
 
   await api
-    .post('/api/blogs')
+    .post("/api/blogs")
     .send(newBlog)
     .expect(201)
-    .expect('Content-Type', /application\/json/)
+    .expect("Content-Type", /application\/json/);
 
-  const response = await api.get('/api/blogs')
+  const response = await api.get("/api/blogs");
 
-  const titles = response.body.map(r => r.title)
+  const titles = response.body.map((r) => r.title);
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
-  expect(titles).toContain(
-    'new title test'
-  )
-})
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1);
+  expect(titles).toContain("new title test");
+});
 
 afterAll(async () => {
   await mongoose.connection.close();
