084cbf7f31c0d3a8735ccb359343094ed23c5e07
iltan2 <55798506+iltan2@users.noreply.github.com>
2023-04-13 22:58:24 +0800

    Format models/blog.js

--- models/blog.js
+++ models/blog.js
@@ -1,26 +1,26 @@
-const config = require('../utils/config')
-const logger = require('../utils/logger')
-const mongoose = require('mongoose')
+const config = require("../utils/config");
+const logger = require("../utils/logger");
+const mongoose = require("mongoose");
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
-
+    logger.error("error connecting to MongoDB:", error.message);
+  });
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
-})
+  likes: Number,
+});
 
-module.exports = mongoose.model('Blog', blogSchema)
 
+module.exports = mongoose.model("Blog", blogSchema);
