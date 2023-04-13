150104581077576691e832342601f77b0d858515
iltan2 <55798506+iltan2@users.noreply.github.com>
2023-04-13 23:11:49 +0800

    Add test to check if likes defaults to 0

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -72,6 +72,26 @@ test("a valid blog can be added", async () => {
   expect(titles).toContain("new title test");
 });
 
+test("default likes is 0", async () => {
+  const newBlog = {
+    title: "new title test 2",
+    author: "new author test 2",
+    url: "new url test 2",
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/blogs");
+  const blogLikes = response.body.find((r)=> r.title==="new title test 2").likes;
+
+  expect(blogLikes).toBe(0);
+});
+
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
