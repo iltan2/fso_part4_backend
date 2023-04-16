ebd1f01765440c00aa2919d35b4b255fb90866dc
iltan2 <israeltan2@gmail.com>
2023-04-16 12:17:04 +0800

    Add test for uniqueness and formatting

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,27 @@
-const bcrypt = require('bcrypt')
-const usersRouter = require('express').Router()
-const User = require('../models/user')
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
 
 usersRouter.get("/", async (request, response) => {
-    const users = await User.find({});
-    response.json(users);
+  const users = await User.find({});
+  response.json(users);
+});
+
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
   });
-  
-usersRouter.post('/', async (request, response) => {
-    const { username, name, password } = request.body
-  
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
-  
-    const user = new User({
-      username,
-      name,
-      passwordHash,
-    })
-  
-    const savedUser = await user.save()
-  
-    response.status(201).json(savedUser)
-  })
 
+  const savedUser = await user.save();
 
-module.exports = usersRouter
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
