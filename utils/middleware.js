e2cd8bfea6a5cb76035414fe045e72aa356442de
iltan2 <israeltan2@gmail.com>
2023-04-16 21:12:32 +0800

    Format middleware.js

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,12 +1,12 @@
-const logger = require('./logger')
+const logger = require("./logger");
 
 const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method)
-  logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
-  logger.info('---')
-  next()
-}
+  logger.info("Method:", request.method);
+  logger.info("Path:  ", request.path);
+  logger.info("Body:  ", request.body);
+  logger.info("---");
+  next();
+};
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
@@ -29,5 +29,5 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
-}
+  errorHandler,
+};
