4461d6c120648d38e61c7e1eee8165543fc32abf
Israel Tan <israeltan2@gmail.com>
2023-04-17 00:56:38 +0800

    Update to complete exercise 4.22

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -15,12 +15,8 @@ blogsRouter.get("/", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
+  const user = await request.user;
   const body = request.body;
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token invalid" });
-  }
-  const user = await User.findById(decodedToken.id);
   const blog = new Blog({
     title: body.title,
     author: body.author,
@@ -36,11 +32,7 @@ blogsRouter.post("/", async (request, response) => {
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token invalid" });
-  }
-  const user = await User.findById(decodedToken.id);
+  const user = await request.user;
   const blog = await Blog.findById(request.params.id);
 
   if (blog.user.toString() === user.id.toString()) {
