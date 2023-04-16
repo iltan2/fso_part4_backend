887ad8aa84ab299f19833794f1998bd89155607c
iltan2 <israeltan2@gmail.com>
2023-04-16 18:44:03 +0800

    Update for exercise 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,14 +4,19 @@ const User = require("../models/user");
 
 console.log("loading the blog");
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
+
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
   const users = await User.find({});
-  const user = users[0]
+  const user = users[0];
 
   const blog = new Blog({
     title: body.title,
